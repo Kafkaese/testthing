5d41766130823b4d6b97e0024f7e908135b616c3
Frederik Laubisch <kafkaese@gmail.com>
2021-02-21 16:46:32 +0100

    second test

--- mlproject/distance.py
+++ mlproject/distance.py
@@ -20,6 +20,6 @@ if __name__ == "__main__":
     # Le Wagon location
     lat1, lon1 = 48.865070, 2.380009
     #Insert your coordinates from google maps here
-    lat2, lon2 = 47.0, 12.0
+    lat2, lon2 = 47.0, 11.0
     distance = haversine(lon1, lat1, lon2, lat2)
     print(distance)
